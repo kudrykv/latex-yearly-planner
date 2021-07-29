f810710cae82182b2b88715187b6db13cf26469a
Vitaliy Kudryk <kudryk.v@protonmail.com>
2021-07-29 20:37:44 +0300

    quick & dirty for first week

--- scripts/3_monthly.js
+++ scripts/3_monthly.js
@@ -11,7 +11,7 @@ const monthly = (year, month) => {
   const date = new Date(year, month, 1);
   let startingWeek = moment(date);
   calendar.forEach(row => {
-    row.unshift(rotateWeek(startingWeek.isoWeek()));
+    row.unshift(rotateWeek(month, startingWeek.isoWeek()));
     startingWeek.add(1, 'week');
   })
 
@@ -19,25 +19,31 @@ const monthly = (year, month) => {
   const weekdays = getWeekdays().map(day => `\\hfil ${day}`).join(' & ');
   const leftList = [
     ls.slink(year),
-    ls.slink(`Q${Math.floor(month / 3)+1}`),
+    ls.slink(`Q${Math.floor(month / 3) + 1}`),
     ls.starget(moment(date).format('MMMM'))
   ];
 
   const rightList = [];
   if (month > 0) {
-    const prevMonth = moment(new Date(year, month-1, 1)).format('MMMM');
+    const prevMonth = moment(new Date(year, month - 1, 1)).format('MMMM');
     rightList.push(ls.slink(prevMonth))
   }
 
   if (month < 11) {
-    const nextMonth = moment(new Date(year, month+1, 1)).format('MMMM');
+    const nextMonth = moment(new Date(year, month + 1, 1)).format('MMMM');
     rightList.push(ls.slink(nextMonth))
   }
 
   return `${ls.header(leftList, rightList)}\n${funcs.interpolateTpl('monthly', {weekdays, calendar})}\n`;
 }
 
-const rotateWeek = weekNum => funcs.interpolateTpl('rotatedWeekNum', {weekNum: ls.slink('Week ' + weekNum)})
+const rotateWeek = (month, weekNum) => {
+  const link = month === 0 && weekNum > 50
+    ? ls.link('fwWeek ' + weekNum, 'Week ' + weekNum)
+    : ls.slink('Week ' + weekNum);
+
+  return funcs.interpolateTpl('rotatedWeekNum', {weekNum: link})
+}
 
 const getWeekdays = () => ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
