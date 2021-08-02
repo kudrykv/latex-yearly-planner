bb5b802a8a1f22ec83bae4e326747fcd568ce36e
Vitaliy Kudryk <kudryk.v@protonmail.com>
2021-08-02 19:18:23 +0300

    upd quarterly

--- scripts/30_quarterly.js
+++ scripts/30_quarterly.js
@@ -2,20 +2,16 @@ const funcs = require('./funcs');
 const act = require('./20_annual');
 const ls = require('./latexsnips');
 
-const qtabular = (selected) => {
-  selected++;
-
-  return funcs.range(1, 5)
-    .map(num => num === selected ? ls.target('Q' + num, '\\textbf{Q' + num + '}') : ls.slink('Q' + num))
+const qtabular = (selected) =>
+  funcs.range(1, 5)
+    .map(num => num === selected + 1 ? ls.target('Q' + num, '\\textbf{Q' + num + '}') : ls.slink('Q' + num))
     .join('\\quad{}');
-}
+
 
 const quarter = (year, q) => {
-  const tabulars = funcs.range(q * 3, q * 3 + 3).map(qq => {
-    const calendar = act.monthTabular(year, qq);
-
-    return funcs.interpolateTpl('qrtrRow', {calendar});
-  })
+  const tabulars = funcs
+    .range(q * 3, q * 3 + 3)
+    .map(qq => funcs.interpolateTpl('qrtrRow', {calendar: act.monthTabular(year, qq)}));
 
   const rightList = [ls.link('To Do Index', 'Todos'), ls.link('Notes Index', 'Notes')];
   return `${ls.header([ls.slink(year), qtabular(q)], rightList)}
