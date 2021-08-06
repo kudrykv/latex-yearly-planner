58e5d7e2df07bee1c00356a73e4ed095cc59ca60
Vitaliy Kudryk <kudryk.v@protonmail.com>
2021-08-06 16:16:21 +0300

    quarterlies

--- planner/supernote_a5x/30_quarterly.ts
+++ planner/supernote_a5x/30_quarterly.ts
@@ -1,22 +1,22 @@
-const funcs = require('../common/funcs');
-const act = require('./20_annual');
-const ls = require('../common/latexsnips');
+const {range, interpolateTpl} = require('../common/funcs');
+const {monthlyTabular, link, header, slink, target} = require('../common/latexsnips');
 
-export const qtabular = (selected) =>
-  funcs.range(1, 5)
-    .map(num => num === selected + 1 ? ls.target('Q' + num, '\\textbf{Q' + num + '}') : ls.slink('Q' + num))
-    .join('\\quad{}');
+export const quarter = ({
+  year,
+  q,
+  weeks = true,
+  weekStart = 1
+}: { year: number, q: number, weeks?: boolean, weekStart?: 1 | 7 }) => {
+  const llist = [slink(year), qtabular(q)];
+  const rightList = [link('To Do Index', 'Todos'), link('Notes Index', 'Notes')];
+  const tabulars = range((q - 1) * 3 + 1, (q - 1) * 3 + 4)
+    .map(qq => interpolateTpl('qrtrRow', {calendar: monthlyTabular({year, month: qq, weeks, weekStart})}));
 
-
-export const quarter = (year, q) => {
-  // const tabulars = funcs
-  //   .range(q * 3, q * 3 + 3)
-  //   .map(qq => funcs.interpolateTpl('qrtrRow', {calendar: act.monthTabular(year, qq)}));
-
-  const rightList = [ls.link('To Do Index', 'Todos'), ls.link('Notes Index', 'Notes')];
-  return `${ls.header([ls.slink(year), qtabular(q)], rightList)}
-
-tbd
-\\pagebreak
-`
+  return `${header(llist, rightList)}\n\n${tabulars.join('\\vfill')}\\pagebreak\n`
 }
+
+
+const qtabular = (selected) =>
+  range(1, 5)
+    .map(num => num === selected ? target('Q' + num, '\\textbf{Q' + num + '}') : slink('Q' + num))
+    .join('\\quad{}');
