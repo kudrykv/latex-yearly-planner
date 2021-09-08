f1fce32e0340106fe25e967b64ee1f3b63a201bd
Vitaliy Kudryk <kudryk.v@protonmail.com>
2021-09-08 10:28:28 +0300

    cfg

--- release.sh
+++ release.sh
@@ -3,18 +3,6 @@ NEXT_YEAR=$((CURRENT_YEAR+1))
 
 for year in $CURRENT_YEAR $NEXT_YEAR; do
   for cfg in cfg/*yaml; do
-    PLANNER_YEAR=${year} go run cmd/plannergen/plannergen.go --config "${cfg}"
-
-    nakedname=$(echo "${cfg}" | cut -d'/' -f 2-99 | rev | cut -d'.' -f 2-99 | rev)
-
-    pdflatex \
-      -file-line-error \
-      -interaction=nonstopmode \
-      -synctex=1 \
-      -output-format=pdf \
-      -output-directory=./out \
-      "out/${nakedname}.tex"
-
-    cp "out/${nakedname}.pdf" "${year}.${nakedname}.pdf"
+    PLANNER_YEAR=${year} CFG="${cfg}" ./single.sh
   done
 done
