8da7a99a36686109d88ba75e381956c22a5c595b
Vitaliy Kudryk <kudryk.v@protonmail.com>
2021-09-08 09:58:10 +0300

    upd release

--- release.sh
+++ release.sh
@@ -1,10 +1,9 @@
-CURRENT_YEAR=$(date +"%Y")
-NEXT_YEAR=$((CURRENT_YEAR+1))
+PLANNER_YEAR=${PLANNER_YEAR:-$(date +"%Y")}
+export PLANNER_YEAR
 
-for year in $CURRENT_YEAR $NEXT_YEAR; do
-  ./build.sh --planner-year "${year}" --vendor rm2_ddvk --left-handed true
-  ./build.sh --planner-year "${year}" --vendor rm2_ddvk
-  ./build.sh --planner-year "${year}" --vendor rm2_vanilla
-
-  ./build.sh --planner-year "${year}" --vendor supernote_a5x
+for cfg in cfg/*yaml; do
+  go run cmd/plannergen/plannergen.go --config "${cfg}"
+  nakedname=$(echo "${cfg}" | cut -d'/' -f 2-99 | rev | cut -d'.' -f 2-99 | rev)
+  pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=./out "out/${nakedname}.tex"
+  cp "out/${nakedname}.pdf" "${nakedname}.pdf"
 done
