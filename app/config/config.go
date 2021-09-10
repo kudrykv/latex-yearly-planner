09ecc3c119120bccccd0a23a8bb5dce687de0b5c
Vitaliy Kudryk <kudryk.v@protonmail.com>
2021-09-10 20:07:43 +0300

    config

--- app/config/config.go
+++ app/config/config.go
@@ -39,66 +39,6 @@ func (r RenderBlocks) WeeklyEnabled() bool {
 	return false
 }
 
-type Blocks struct {
-	Title        Title
-	Annual       Annual
-	Quarterly    Quarterly
-	Monthly      Monthly
-	Weekly       Weekly
-	Daily        Daily
-	DailyWMonth  DailyWMonth
-	DailyReflect DailyReflect
-	DailyNotes   DailyNotes
-	NotesIndexed NotesIndexed
-	TodosIndexed TodosIndexed
-}
-
-type Title struct {
-	Tpl string
-}
-
-type Annual struct {
-	Tpl string
-}
-
-type Quarterly struct {
-	Tpl string
-}
-
-type Monthly struct {
-	Tpl string
-}
-
-type Weekly struct {
-	Tpl string
-}
-
-type Daily struct {
-	Tpl string
-}
-
-type DailyWMonth struct {
-	Tpl string
-}
-
-type DailyReflect struct {
-	Tpl string
-}
-
-type DailyNotes struct {
-	Tpl string
-}
-
-type NotesIndexed struct {
-	TplIndex string
-	TplPage  string
-}
-
-type TodosIndexed struct {
-	TplIndex string
-	TplPage  string
-}
-
 type Colors struct {
 	Gray      string
 	LightGray string
