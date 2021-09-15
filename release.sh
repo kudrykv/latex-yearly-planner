6cff2eda0b12c82c877c68cc8c3e3f85ed90887c
Vitaliy Kudryk <kudryk.v@protonmail.com>
2021-09-15 18:31:55 +0300

    wip config setup

--- release.sh
+++ release.sh
@@ -1,8 +1,18 @@
 CURRENT_YEAR=$(date +"%Y")
 NEXT_YEAR=$((CURRENT_YEAR+1))
 
-for year in $CURRENT_YEAR $NEXT_YEAR; do
-  for cfg in cfg/*yaml; do
+_sn_a5x="cfg/base.yaml,cfg/base.supernote_a5x.yaml"
+_rm2="cfg/base.yaml,cfg/base.rm2.yaml"
+
+configurations=(
+#  "${_sn_a5x},cfg/template_planner_yearly.yaml,cfg/planner.supernote_a5x.yaml"
+#  "${_rm2},cfg/template_planner_yearly.yaml,cfg/planner.rm2_ddvk.yaml"
+#  "${_sn_a5x},cfg/template_planner_daily_with_cal.yaml,cfg/planner.supernote_a5x.daily-with-cal.yaml"
+  "${_rm2},cfg/template_planner_daily_with_cal.yaml,cfg/planner.rm2_ddvk.daily-with-cal.yaml"
+)
+
+for year in $CURRENT_YEAR; do
+  for cfg in "${configurations[@]}"; do
     PLANNER_YEAR=${year} CFG="${cfg}" ./single.sh
   done
 done
--- single.sh
+++ single.sh
@@ -6,12 +6,16 @@ go run cmd/plannergen/plannergen.go --config "${CFG}"
 
 nakedname=$(echo "${CFG}" | rev | cut -d, -f1 | cut -d'/' -f 1 | cut -d'.' -f 2-99 | rev)
 
-pdflatex \
-  -file-line-error \
-  -interaction=nonstopmode \
-  -synctex=1 \
-  -output-format=pdf \
-  -output-directory=./out \
-  "out/${nakedname}.tex"
+# Even with pdflatex, it requires even more passes.
+# Two should be enough
+for _ in 1 2; do
+  pdflatex \
+    -file-line-error \
+    -interaction=nonstopmode \
+    -synctex=1 \
+    -output-format=pdf \
+    -output-directory=./out \
+    "out/${nakedname}.tex"
+done
 
 cp "out/${nakedname}.pdf" "${PLANNER_YEAR}.${nakedname}.pdf"
