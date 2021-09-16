1a6e3970eac3a12e6fda5221e673fd8efc2b6ef6
Vitaliy Kudryk <kudryk.v@protonmail.com>
2021-09-16 13:55:51 +0300

    upd names

--- release.sh
+++ release.sh
@@ -2,13 +2,13 @@ CURRENT_YEAR=$(date +"%Y")
 NEXT_YEAR=$((CURRENT_YEAR+1))
 
 _sn_a5x="cfg/base.yaml,cfg/base.supernote_a5x.yaml"
-_rm2="cfg/base.yaml,cfg/base.rm2.yaml"
+_rm2="cfg/base.yaml,cfg/rm2_ddvk.base.yaml"
 
 configurations=(
-#  "${_sn_a5x},cfg/template_planner_yearly.yaml,cfg/planner.supernote_a5x.yaml"
-#  "${_rm2},cfg/template_planner_yearly.yaml,cfg/planner.rm2_ddvk.yaml"
-#  "${_sn_a5x},cfg/template_planner_daily_with_cal.yaml,cfg/planner.supernote_a5x.daily-with-cal.yaml"
-  "${_rm2},cfg/template_planner_daily_with_cal.yaml,cfg/planner.rm2_ddvk.daily-with-cal.yaml"
+#  "${_sn_a5x},cfg/template_planner_yearly.yaml,cfg/sn_a5x.planner.yaml"
+#  "${_rm2},cfg/template_planner_yearly.yaml,cfg/rm2_ddvk.planner.yaml"
+#  "${_sn_a5x},cfg/template_planner_daily_with_cal.yaml,cfg/sn_a5x.planner.daily-with-cal.yaml"
+  "${_rm2},cfg/template_planner_daily_with_cal.yaml,cfg/rm2_ddvk.planner.daily-with-cal.yaml"
 )
 
 for year in $CURRENT_YEAR; do
