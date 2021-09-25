7448f5d2353283fc5ca6eeabb90c239f3256c007
Vitaliy Kudryk <kudryk.v@protonmail.com>
2021-09-25 10:00:38 +0300

    upd

--- release.sh
+++ release.sh
@@ -9,14 +9,14 @@ _rm2_ddvk_lh="cfg/base.yaml,cfg/rm2_ddvk_lh.base.yaml"
 
 _configurations=(
   1 "${_rm2},cfg/template_planner_yearly.yaml,cfg/rm2.planner.yaml"
-#  2 "${_rm2},cfg/template_planner_daily_with_cal.yaml,cfg/rm2.planner.daily-with-cal.yaml"
+  2 "${_rm2},cfg/template_planner_daily_with_cal.yaml,cfg/rm2.planner.daily-with-cal.yaml"
   1 "${_rm2_ddvk},cfg/template_planner_yearly.yaml,cfg/rm2_ddvk.planner.yaml"
-#  2 "${_rm2_ddvk},cfg/template_planner_daily_with_cal.yaml,cfg/rm2_ddvk.planner.daily-with-cal.yaml"
+  2 "${_rm2_ddvk},cfg/template_planner_daily_with_cal.yaml,cfg/rm2_ddvk.planner.daily-with-cal.yaml"
   1 "${_rm2_ddvk_lh},cfg/template_planner_yearly.yaml,cfg/rm2_ddvk_lh.planner.yaml"
-#  2 "${_rm2_ddvk_lh},cfg/template_planner_daily_with_cal.yaml,cfg/rm2_ddvk_lh.planner.daily-with-cal.yaml"
+  2 "${_rm2_ddvk_lh},cfg/template_planner_daily_with_cal.yaml,cfg/rm2_ddvk_lh.planner.daily-with-cal.yaml"
   1 "${_sn_a5x},cfg/template_planner_yearly.yaml,cfg/sn_a5x.planner.yaml"
-#  2 "${_sn_a5x},cfg/template_planner_daily_with_cal.yaml,cfg/sn_a5x.planner.daily-with-cal.yaml"
-#  2 "${_sn_a5x_lh},cfg/template_planner_daily_with_cal.yaml,cfg/sn_a5x_lh.planner.daily-with-cal.yaml"
+  2 "${_sn_a5x},cfg/template_planner_daily_with_cal.yaml,cfg/sn_a5x.planner.daily-with-cal.yaml"
+  2 "${_sn_a5x_lh},cfg/template_planner_daily_with_cal.yaml,cfg/sn_a5x_lh.planner.daily-with-cal.yaml"
 )
 
 _configurations_len=${#_configurations[@]}
