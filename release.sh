1140b8973d6d8391253b655e482de83106f05448
Vitaliy Kudryk <kudryk.v@protonmail.com>
2021-11-15 13:52:22 +0200

    upd

--- release.sh
+++ release.sh
@@ -4,25 +4,17 @@ CURRENT_YEAR=$(date +"%Y")
 NEXT_YEAR=$((CURRENT_YEAR+1))
 
 _configurations=(
-#  1 "${_rm2},cfg/template_planner_yearly.yaml,cfg/rm2.planner.yaml"
-#  2 "${_rm2},cfg/template_planner_daily_with_cal.yaml,cfg/rm2.planner.daily-with-cal.yaml"
-#  1 "${_rm2_ddvk},cfg/template_planner_yearly.yaml,cfg/rm2_ddvk.planner.yaml"
-#  2 "${_rm2_ddvk},cfg/template_planner_daily_with_cal.yaml,cfg/rm2_ddvk.planner.daily-with-cal.yaml"
-#  1 "${_rm2_ddvk_lh},cfg/template_planner_yearly.yaml,cfg/rm2_ddvk_lh.planner.yaml"
-#  2 "${_rm2_ddvk_lh},cfg/template_planner_daily_with_cal.yaml,cfg/rm2_ddvk_lh.planner.daily-with-cal.yaml"
-#  1 "${_sn_a5x},cfg/template_planner_yearly.yaml,cfg/sn_a5x.planner.yaml"
-#  2 "${_sn_a5x},cfg/template_planner_daily_with_cal.yaml,cfg/sn_a5x.planner.daily-with-cal.yaml"
-#  2 "${_sn_a5x_lh},cfg/template_planner_daily_with_cal.yaml,cfg/sn_a5x_lh.planner.daily-with-cal.yaml"
-
-  # new
-  1 "cfg/base.yaml,cfg/template_breadcrumb.yaml,cfg/sn_a5x.breadcrumb.default.yaml"                "sn_a5x.breadcrumb.default"
-  2 "cfg/base.yaml,cfg/template_months_on_side.yaml,cfg/sn_a5x.mos.default.yaml"                   "sn_a5x.mos.default"
+  1 "cfg/base.yaml,cfg/template_breadcrumb.yaml,cfg/sn_a5x.breadcrumb.default.yaml" "sn_a5x.breadcrumb.default"
+  2 "cfg/base.yaml,cfg/template_months_on_side.yaml,cfg/sn_a5x.mos.default.yaml"    "sn_a5x.mos.default"
 
   1 "cfg/base.yaml,cfg/rm2.base.yaml,cfg/template_breadcrumb.yaml,cfg/rm2.breadcrumb.default.yaml" "rm2.breadcrumb.default"
   2 "cfg/base.yaml,cfg/rm2.base.yaml,cfg/template_months_on_side.yaml,cfg/rm2.mos.default.yaml"    "rm2.mos.default"
 
   1 "cfg/base.yaml,cfg/rm2_ddvk.base.yaml,cfg/template_breadcrumb.yaml,cfg/rm2.breadcrumb.default.yaml" "rm2_ddvk.breadcrumb.default"
   2 "cfg/base.yaml,cfg/rm2_ddvk.base.yaml,cfg/template_months_on_side.yaml,cfg/rm2.mos.default.yaml"    "rm2_ddvk.mos.default"
+
+  1 "cfg/base.yaml,cfg/rm2_ddvk_lh.base.yaml,cfg/template_breadcrumb.yaml,cfg/rm2.breadcrumb.default.yaml" "rm2_ddvk_lh.breadcrumb.default"
+  2 "cfg/base.yaml,cfg/rm2_ddvk_lh.base.yaml,cfg/template_months_on_side.yaml,cfg/rm2.mos.default.yaml"    "rm2_ddvk_lh.mos.default"
 )
 
 _configurations_len=${#_configurations[@]}
