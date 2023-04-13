d2a144df1f7cd6c89ac0d2df125a6a72e4eac3a7
Vitaliy Kudryk <vitaliy@bgnfu7re.me>
2023-04-13 20:55:37 +0300

    wire first template 🚧

--- internal/core/plannerbuilders/mos/mos.go
+++ internal/core/plannerbuilders/mos/mos.go
@@ -1,3 +1,12 @@
 package mos
 
+import (
+	"context"
+	"github.com/kudrykv/latex-yearly-planner/internal/core/planners/entities"
+)
+
 type MOS struct{}
+
+func (r MOS) Generate(_ context.Context) (entities.FileStructure, error) {
+	panic("not implemented")
+}
