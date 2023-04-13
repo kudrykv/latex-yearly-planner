544cc4932c9af5edc63d310ae8ecc8a2db92963e
Vitaliy Kudryk <vitaliy@bgnfu7re.me>
2023-04-13 18:34:32 +0300

    wiring 🚧

---
+++ cmd/app/app.go
@@ -0,0 +1,30 @@
+package app
+
+import (
+	"context"
+	"fmt"
+	"github.com/urfave/cli/v2"
+	"io"
+)
+
+type App struct {
+	app cli.App
+}
+
+func New(reader io.Reader, writer, errWriter io.Writer) App {
+	return App{
+		app: cli.App{
+			Reader:    reader,
+			Writer:    writer,
+			ErrWriter: errWriter,
+		},
+	}
+}
+
+func (r App) Run(ctx context.Context, arguments []string) error {
+	if err := r.app.RunContext(ctx, arguments); err != nil {
+		return fmt.Errorf("run context: %w", err)
+	}
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -2,8 +2,4 @@ module github.com/kudrykv/latex-yearly-planner
 
 go 1.16
 
-require (
-	github.com/caarlos0/env/v6 v6.7.0
-	github.com/urfave/cli/v2 v2.3.0
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
-)
+require github.com/urfave/cli/v2 v2.3.0
