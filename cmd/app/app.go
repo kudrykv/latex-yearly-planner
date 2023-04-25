2baa7dbaf0185ddabc4925e7fa606393c2e78a53
Vitaliy Kudryk <vitaliy@bgnfu7re.me>
2023-04-25 19:55:57 +0300

    wip cmd

--- cmd/app/app.go
+++ cmd/app/app.go
@@ -3,6 +3,12 @@ package app
 import (
 	"context"
 	"fmt"
+	"github.com/kudrykv/latex-yearly-planner/internal/adapters/commander"
+	"github.com/kudrykv/latex-yearly-planner/internal/adapters/filewriter"
+	"github.com/kudrykv/latex-yearly-planner/internal/adapters/mos/mostitle"
+	"github.com/kudrykv/latex-yearly-planner/internal/adapters/texindexer"
+	"github.com/kudrykv/latex-yearly-planner/internal/core/plannerbuilders"
+	"github.com/kudrykv/latex-yearly-planner/internal/core/planners"
 	"github.com/urfave/cli/v2"
 	"io"
 )
@@ -25,6 +31,20 @@ func New(reader io.Reader, writer, errWriter io.Writer) App {
 						{
 							Name: "mos",
 							Action: func(cliContext *cli.Context) error {
+								fileWriter := filewriter.New("./out")
+								cmder := commander.New("./out")
+
+								indexer, err := texindexer.New("")
+								if err != nil {
+									return fmt.Errorf("new indexer: %w", err)
+								}
+
+								titleSection := mostitle.New(mostitle.TitleParameters{})
+
+								builder := plannerbuilders.New(indexer, plannerbuilders.Sections{titleSection})
+
+								planners.New(builder, fileWriter, cmder)
+
 								panic("not implemented")
 							},
 						},
