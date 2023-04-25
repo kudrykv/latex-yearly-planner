f61a3254d725ace0309feabb97bb58c996d5bc8f
Vitaliy Kudryk <vitaliy@bgnfu7re.me>
2023-04-25 19:53:17 +0300

    dummy title

---
+++ internal/adapters/mos/mostitle/title.go
@@ -0,0 +1,31 @@
+package mostitle
+
+import (
+	"context"
+	"github.com/kudrykv/latex-yearly-planner/internal/core/entities"
+)
+
+type TitleParameters struct {
+	IsEnabled bool
+
+	Name string
+}
+
+type Title struct {
+	parameters TitleParameters
+}
+
+func New(parameters TitleParameters) Title {
+	return Title{parameters: parameters}
+}
+
+func (r Title) IsEnabled() bool {
+	return r.parameters.IsEnabled
+}
+
+func (r Title) GenerateSection(_ context.Context) (entities.Note, error) {
+	return entities.Note{
+		Name:     "title.tex",
+		Contents: []byte(r.parameters.Name),
+	}, nil
+}
