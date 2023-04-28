7ad8ca06063541bbbf71dabbe8997066486ec102
Vitaliy Kudryk <vitaliy@bgnfu7re.me>
2023-04-28 17:59:27 +0300

    experiment

--- internal/adapters/mos/sections/mosannual/section.go
+++ internal/adapters/mos/sections/mosannual/section.go
@@ -28,5 +28,30 @@ func (r Section) IsEnabled() bool {
 }
 
 func (r Section) GenerateSection(_ context.Context) (entities.Note, error) {
+	if err := r.makeHeader(); err != nil {
+		return entities.Note{}, fmt.Errorf("make header: %w", err)
+	}
+
+	if err := r.makeBody(); err != nil {
+		return entities.Note{}, fmt.Errorf("make body: %w", err)
+	}
+
+	note, err := r.makeNote()
+	if err != nil {
+		return entities.Note{}, fmt.Errorf("make note: %w", err)
+	}
+
+	return note, nil
+}
+
+func (r Section) makeHeader() error {
+	return fmt.Errorf("not implemented")
+}
+
+func (r Section) makeBody() error {
+	return fmt.Errorf("not implemented")
+}
+
+func (r Section) makeNote() (entities.Note, error) {
 	return entities.Note{}, fmt.Errorf("not implemented")
 }
