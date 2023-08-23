66c0c6d53e4dafede3efe31c1ec372b1848b9928
Vitaliy Kudryk <vitaliy@bgnfu7re.me>
2023-08-23 10:13:51 +0300

    config

--- lib/latex_yearly_planner/pieces/mos/components/annual_body.rb
+++ lib/latex_yearly_planner/pieces/mos/components/annual_body.rb
@@ -6,39 +6,27 @@ module LatexYearlyPlanner
       module Components
         class AnnualBody < Component
           def generate(months)
-            page = ''
-            months_number = months.size
-
-            months.each_slice(section_config.parameters.columns).each do |slice|
-              page += slice.map do |month|
-                LatexYearlyPlanner::XTeX::CalendarLittle.new(month, **calendar_options)
-              end.map(&:to_s).join('\\hfill{}')
-              page += separator(months_number)
+            rows = months.each_slice(section_config.parameters.columns).map do |slice|
+              slice
+                .map { |month| LatexYearlyPlanner::XTeX::CalendarLittle.new(month, **calendar_options) }
+                .map(&:to_s)
+                .join('\\hfill{}')
             end
 
-            page
+            months_number = months.size
+            rows.join(separator(months_number))
           end
 
           private
 
           def calendar_options
-            {
-              width: column_width,
-              show_week_numbers: param(:show_week_numbers),
-              week_number_placement: param(:week_number_placement),
-              row_spacing: config.parameters.parameters.little_calendar.row_spacing,
-              column_spacing: config.parameters.parameters.little_calendar.column_spacing
-            }
+            config.little_calendar(section_name)
           end
 
           def separator(months_number)
             return "\n\\vfill{}\n\n" if months_number == months_per_page
 
             "\n\\vspace{#{underfull_vertical_spacing}}\n\n"
-          end
-
-          def column_width
-            param(:column_width)
           end
 
           def months_per_page
--- lib/latex_yearly_planner/pieces/mos/config.rb
+++ lib/latex_yearly_planner/pieces/mos/config.rb
@@ -17,6 +17,31 @@ module LatexYearlyPlanner
         def method_missing(...)
           struct.method_missing(...)
         end
+
+        def little_calendar(section_name)
+          base = {
+            show_week_numbers: struct.parameters.parameters.show_week_numbers,
+            week_number_placement: struct.parameters.parameters.week_number_placement,
+          }.merge(struct.parameters.parameters.little_calendar_as_a_hash)
+
+          if section(section_name).parameters.show_week_numbers
+            base = base.merge({ show_week_numbers: section(section_name).parameters.show_week_numbers })
+          end
+
+          if section(section_name).parameters.week_number_placement
+            base = base.merge({ week_number_placement: section(section_name).parameters.week_number_placement })
+          end
+
+          if section(section_name).parameters.little_calendar_as_a_hash
+            base = base.merge(section(section_name).parameters.little_calendar_as_a_hash)
+          end
+
+          base
+        end
+
+        def section(section)
+          struct.sections.send(section)
+        end
       end
     end
   end
