4988e26c94b85db5ebe6c2edad28534d2e4af8a4
Vitaliy Kudryk <vitaliy@bgnfu7re.me>
2023-08-23 22:08:26 +0300

    experiment

--- lib/latex_yearly_planner/pieces/mos/components/quarterly_body.rb
+++ lib/latex_yearly_planner/pieces/mos/components/quarterly_body.rb
@@ -6,7 +6,7 @@ module LatexYearlyPlanner
       module Components
         class QuarterlyBody < Component
           def generate(quarter)
-            "#{calendars_minipage(quarter)}\\leaders hello"
+            "#{calendars_minipage(quarter)} hello"
           end
 
           private
