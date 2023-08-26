6b454e3bc7173043c8cd13f09f2fd612a6c6b9d2
Vitaliy Kudryk <vitaliy@bgnfu7re.me>
2023-08-26 22:42:44 +0300

    experiment

--- lib/latex_yearly_planner/pieces/mos/components/daily_reflect_body.rb
+++ lib/latex_yearly_planner/pieces/mos/components/daily_reflect_body.rb
@@ -7,7 +7,9 @@ module LatexYearlyPlanner
         class DailyReflectBody < Component
           def generate(_day)
             [
-              goals_label, hfill, best_thing_label, nl, goals_and_best_thing_notes
+              goals_label, hfill, best_thing_label, nl, goals_and_best_thing_notes, nlnl,
+              gratitude_label, nl, gratitude_notes, nlnl,
+              daily_log_label, nl, daily_log_notes
             ].join
           end
 
@@ -23,6 +25,22 @@ module LatexYearlyPlanner
 
           def goals_and_best_thing_notes
             XTeX::Notes.new(**struct(:goals_and_best_thing_notes))
+          end
+
+          def gratitude_label
+            XTeX::Label.new(**struct(:gratitude_label).deep_merge({ parameters: { text: 'Things I\'m grateful for' } }))
+          end
+
+          def gratitude_notes
+            XTeX::Notes.new(**struct(:gratitude_notes))
+          end
+
+          def daily_log_label
+            XTeX::Label.new(**struct(:daily_log_label).deep_merge({ parameters: { text: 'Daily Log' } }))
+          end
+
+          def daily_log_notes
+            XTeX::Notes.new(**struct(:daily_log_notes))
           end
         end
       end
