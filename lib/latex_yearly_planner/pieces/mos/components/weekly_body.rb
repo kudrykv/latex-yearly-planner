543b4ea8f0471c9633eeaca372f3689ec6a49418
Vitaliy Kudryk <vitaliy@bgnfu7re.me>
2023-08-26 13:37:09 +0300

    experiment

--- lib/latex_yearly_planner/pieces/mos/components/weekly_body.rb
+++ lib/latex_yearly_planner/pieces/mos/components/weekly_body.rb
@@ -10,24 +10,20 @@ module LatexYearlyPlanner
               row(week.days.take(3).map(&:name)),
               row(week.days.drop(3).take(3).map(&:name)),
               row([week.days.last.name, 'Notes', XTeX::Dummy.new])
-            ].join('\\vfill{}')
+            ].join(vfill)
           end
 
           private
 
           def row(arr)
-            names = arr.map { |content| col(content) }.join('\\hfill{}')
-            notes = XTeX::Notes.new(notes_type, **parameters(:notes))
+            names = arr.map(&method(:col)).join('\\hfill{}')
+            notes = XTeX::Notes.new(param(:notes, :type), **parameters(:notes))
 
             "#{names}\n#{notes}".strip
           end
 
           def col(content)
             TeX::Parbox.new(content:, width: param(:column_width))
-          end
-
-          def notes_type
-            param(:notes, :type)
           end
         end
       end
