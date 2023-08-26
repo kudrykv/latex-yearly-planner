b9c51cd1e0ce67a8deb33ccc12256185a48e047d
Vitaliy Kudryk <vitaliy@bgnfu7re.me>
2023-08-26 13:02:41 +0300

    experiment

--- lib/latex_yearly_planner/xtex/calendar_large.rb
+++ lib/latex_yearly_planner/xtex/calendar_large.rb
@@ -10,7 +10,7 @@ module LatexYearlyPlanner
                   :vertical_stretch,
                   :horizontal_spacing,
                   :horizontal_lines,
-                  :weekday_start
+                  :name_cell_height
 
       def initialize(month, **options)
         @month = month
@@ -19,8 +19,9 @@ module LatexYearlyPlanner
         @week_number_placement = options.fetch(:week_number_placement, :left).downcase.to_sym
         @cell_height = options.fetch(:cell_height, '2cm')
         @vertical_stretch = options.fetch(:vertical_stretch, 1.0)
-        @horizontal_spacing = options.fetch(:horizontal_spacing, 0.5)
+        @horizontal_spacing = options.fetch(:horizontal_spacing, nil)
         @horizontal_lines = options.fetch(:horizontal_lines, true)
+        @name_cell_height = options.fetch(:name_cell_height, '5mm')
       end
 
       def to_s
@@ -51,6 +52,7 @@ module LatexYearlyPlanner
 
       def header
         row = month.weekdays_short.map { |day| "\\hfil{}#{day}" }
+        row[0] = "#{MinHeight.new(name_cell_height)}#{row[0]}"
 
         return row unless show_week_numbers
 
@@ -62,6 +64,8 @@ module LatexYearlyPlanner
       def week_rows
         month.weeks.map do |week|
           row = week.days.map { |day| day ? "{#{day.mday}}" : '' }
+          row[0] = "\\adjustbox{valign=t}{\\vphantom{\\rule{0pt}{#{cell_height}}}}#{row[0]}"
+
           next row unless show_week_numbers
 
           row.unshift(rotated(week)) if week_number_placement == :left
@@ -72,7 +76,7 @@ module LatexYearlyPlanner
       end
 
       def rotated(week)
-        "\\vspace{0pt}\\rotatebox[origin=tr]{90}{\\parbox{#{cell_height}}{\\hfil{}#{week_name_box(week)}}}"
+        "\\rotatebox[origin=tr]{90}{\\parbox{#{cell_height}}{\\hfil{}#{week_name_box(week)}}}"
       end
 
       def week_name_box(week)
