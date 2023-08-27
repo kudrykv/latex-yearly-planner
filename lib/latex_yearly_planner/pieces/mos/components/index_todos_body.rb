ada9c9621a45bada81b635238d52f67abb289070
Vitaliy Kudryk <vitaliy@bgnfu7re.me>
2023-08-27 14:39:08 +0300

    experiment

--- lib/latex_yearly_planner/pieces/mos/components/index_todos_body.rb
+++ lib/latex_yearly_planner/pieces/mos/components/index_todos_body.rb
@@ -6,11 +6,29 @@ module LatexYearlyPlanner
       module Components
         class IndexTodosBody < Component
           def generate_index(page)
-            'hello world'
+            XTeX::IndexTable.new(**struct(:index_table).deep_merge(additional_index_params(page)))
           end
 
           def generate_todo(_todo, _page)
-            'hello world'
+            [
+              TeX::Minipage.new(
+                content: XTeX::ToDo.new(**parameters(:todos)),
+                width: param(:column_width),
+              ),
+              hfill,
+              TeX::Minipage.new(
+                content: XTeX::ToDo.new(**parameters(:todos)),
+                width: param(:column_width),
+              ),
+            ].join
+          end
+
+          def additional_index_params(page)
+            { parameters: { start_from: start_from(page), count: param(:todos_per_page) } }
+          end
+
+          def start_from(page)
+            ((page - 1) * param(:todos_per_page)) + 1
           end
         end
       end
