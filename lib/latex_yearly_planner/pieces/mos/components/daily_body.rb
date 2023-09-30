d4b6a28900428e9fa71000bc774e78f83f042693
Vitaliy Kudryk <vitaliy@bgnfu7re.me>
2023-09-30 12:52:32 +0300

    code

--- lib/latex_yearly_planner/pieces/mos/components/daily_body.rb
+++ lib/latex_yearly_planner/pieces/mos/components/daily_body.rb
@@ -72,7 +72,7 @@ module LatexYearlyPlanner
 
           def personal_notes_label(day)
             arr = ['Personal Notes']
-            arr.push(hfill, link_daily_notes('More', day:)) if config.sections.daily_reflect.enabled
+            arr.push(hfill, link_reflect('Reflect', day:)) if config.sections.daily_reflect.enabled
             text = arr.join
 
             XTeX::Label.new(**struct(:personal_notes_label).deep_merge({ parameters: { text: } }))
