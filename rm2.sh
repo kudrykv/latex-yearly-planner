351af947e6d4950a554921b0940521817bc83478
Cameron Will <stephen.will@tanium.com>
2023-11-17 21:29:35 -0500

    feat: Add shell script for my default

---
+++ rm2.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+PLANNER_YEAR=2024 PASSES=1 CFG="cfg/base.yaml,cfg/template_breadcrumb.yaml,cfg/rm2.base.yaml,cfg/rm2.breadcrumb.default.dailycal.yaml" NAME="rm2.breadcrumb" ./single.sh
