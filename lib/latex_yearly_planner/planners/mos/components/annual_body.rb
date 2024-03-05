69a0e3b6e6537dc4267425d6b6d203fcf6ec5363
Vitaliy Kudryk <vitaliy@bgnfu7re.me>
2024-03-05 21:08:02 +0200

    stuff

---
+++ lib/latex_yearly_planner/planners/mos/components/annual_body.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module LatexYearlyPlanner
+  module Planners
+    module Mos
+      module Components
+        class AnnualBody < Component
+          def generate(...)
+            'annual body'
+          end
+        end
+      end
+    end
+  end
+end
---
+++ lib/latex_yearly_planner/planners/mos/components/annual_header.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module LatexYearlyPlanner
+  module Planners
+    module Mos
+      module Components
+        class AnnualHeader < Component
+          'annual header'
+        end
+      end
+    end
+  end
+end
