0549355df9384a8cafdad8db578a8084a9a7bb3d
Vitaliy Kudryk <vitaliy@bgnfu7re.me>
2024-03-14 21:58:02 +0200

    stuff

--- lib/latex_yearly_planner/adapters/indexer.rb
+++ lib/latex_yearly_planner/adapters/indexer.rb
@@ -3,11 +3,12 @@
 module LatexYearlyPlanner
   module Adapters
     class Indexer
-      attr_reader :planner_config, :template
+      attr_reader :planner_config, :template, :i18n
 
-      def initialize(planner_config:)
+      def initialize(planner_config:, i18n: I18n)
         @planner_config = planner_config
         @template = ERB.new(File.read('./lib/latex_yearly_planner/erb/document.tex.erb'))
+        @i18n = i18n
       end
 
       def index(text_documents)
@@ -33,6 +34,10 @@ module LatexYearlyPlanner
       def index_text_file(text_documents)
         content = template.result(binding)
         Entities::TextDocument.new(name: 'index.tex', content:)
+      end
+
+      def babel_language
+        i18n.t('babel')
       end
     end
   end
