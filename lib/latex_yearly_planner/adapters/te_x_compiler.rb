ebc6c0b7c3a2c6dfe8dd0b0ee93a9a20820bb4bb
Vitaliy Kudryk <vitaliy@bgnfu7re.me>
2024-03-15 17:03:23 +0200

    stuff

--- lib/latex_yearly_planner/adapters/te_x_compiler.rb
+++ lib/latex_yearly_planner/adapters/te_x_compiler.rb
@@ -4,15 +4,18 @@ require 'English'
 module LatexYearlyPlanner
   module Adapters
     class TeXCompiler
-      attr_reader :workdir
+      attr_reader :workdir, :planner_config
 
-      def initialize(workdir:)
+      def initialize(workdir:, planner_config:)
         @workdir = workdir
+        @planner_config = planner_config
       end
 
       def compile(text_document)
-        `cd #{workdir} && xelatex -halt-on-error #{text_document.name}`
-        raise "Error compiling #{text_document.name}" unless $CHILD_STATUS.success?
+        planner_config.config.compile.runs.times do
+          `cd #{workdir} && xelatex -halt-on-error #{text_document.name}`
+          raise "Error compiling #{text_document.name}" unless $CHILD_STATUS.success?
+        end
       end
     end
   end
--- lib/latex_yearly_planner/high_level/initialize_compiler.rb
+++ lib/latex_yearly_planner/high_level/initialize_compiler.rb
@@ -6,7 +6,7 @@ module LatexYearlyPlanner
       include Interactor
 
       def call
-        context.compiler = Adapters::TeXCompiler.new(workdir:)
+        context.compiler = Adapters::TeXCompiler.new(workdir:, planner_config:)
       end
 
       private
@@ -15,6 +15,12 @@ module LatexYearlyPlanner
         raise DevelopmentError, 'workdir is not defined' unless context.workdir
 
         context.workdir
+      end
+
+      def planner_config
+        raise DevelopmentError, 'planner_config is not defined' unless context.planner_config
+
+        context.planner_config
       end
     end
   end
