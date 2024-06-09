e44d4a94bd4083d777888e55bfe816c6635c2f70
Vitaliy Kudryk <vitaliy@bgnfu7re.me>
2024-06-09 13:23:45 +0300

    stuff

--- lib/latex_yearly_planner/planners/mos/components/quarterly_header.rb
+++ lib/latex_yearly_planner/planners/mos/components/quarterly_header.rb
@@ -5,8 +5,29 @@ module LatexYearlyPlanner
     module Mos
       module Components
         class QuarterlyHeader < HeaderBase
-          def generate(...)
-            'hello quarterly header!'
+          def generate(quarter, _page_number)
+            <<~LATEX.strip
+              \\marginnote{#{in_margin_note}}%
+              #{heading_table(quarter:)}%
+              #{XTeX::Line.new}%
+              \\vskip#{params.get(:header_separation)}
+            LATEX
+          end
+
+          def in_margin_note
+            XTeX::MosSideNav.new(i18n:, struct: params.object(:side_navigation), quarters:, months:)
+          end
+
+          def heading_table(quarter:)
+            XTeX::MosHeadingTable.new(struct: params.object(:heading_table), page_name: page_name(quarter), navigation:)
+          end
+
+          def page_name(quarter)
+            "#{i18n.translate('calendar.one_letter.quarter')}#{quarter.number}"
+          end
+
+          def navigation
+            ['Calendar']
           end
         end
       end
