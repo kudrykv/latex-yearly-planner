b48e05a20c3f3bc50f029993d74dd74eab589167
Cameron Will <cameron@thewills.net>
2024-09-19 21:36:23 -0400

    fix: Build with go and latex directly, forget nix

--- rmpp.sh
+++ rmpp.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-PLANNER_YEAR=2024 PASSES=1 CFG="cfg/base.yaml,cfg/template_breadcrumb.yaml,cfg/rmpp.base.yaml,cfg/rmpp.breadcrumb.default.dailycal.yaml,cfg/test.yaml" NAME="rmpp.breadcrumb" ./single.sh
+PLANNER_YEAR=2024 PASSES=1 CFG="cfg/base.yaml,cfg/template_breadcrumb.yaml,cfg/rmpp.base.yaml,cfg/test.yaml,cfg/rmpp.breadcrumb.default.dailycal.yaml" NAME="rmpp.breadcrumb" ./single.sh
