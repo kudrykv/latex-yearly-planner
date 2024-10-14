ddde4e7f136cb7df507944dd9d15bf951c013295
Vitaliy Kudryk <vitaliy@bgnfu7re.me>
2024-10-14 08:57:58 +0300

    experiment

--- lib/latex_yearly_planner/planners/typst_mos/pages/title.rb
+++ lib/latex_yearly_planner/planners/typst_mos/pages/title.rb
@@ -6,7 +6,19 @@ module LatexYearlyPlanner
       module Pages
         class Title < Page
           def generate(title, _page_number)
-            title.to_s
+            <<~TYPST
+              #block(
+                width: 100%,
+                height: 100%,
+                align(
+                  center + horizon,
+                  text(
+                    size: 48pt,
+                    [#{title}]
+                  )
+                )
+              )
+            TYPST
           end
         end
       end
