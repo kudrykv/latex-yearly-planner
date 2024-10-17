4f891035aa0a6a563e673c6aa9984fb69a7b014d
Vitaliy Kudryk <vitaliy@bgnfu7re.me>
2024-10-17 08:38:15 +0300

    experiment

--- lib/latex_yearly_planner/planners/typst_mos/pages/annual.rb
+++ lib/latex_yearly_planner/planners/typst_mos/pages/annual.rb
@@ -5,51 +5,21 @@ module LatexYearlyPlanner
     module TypstMos
       module Pages
         class Annual < Face
-          def generate(month_rows, _page_number)
-            <<~TYPST
-              #grid(
-                columns: (#{mosnav[:width]}, 1fr),
-                rows: (#{heading[:height]}, 1fr),
-                grid.cell(
-                  rowspan: 2,
-                  pad(right: #{mosnav[:inset]}, #{temp})
-                ),
-                pad(bottom: #{heading[:inset]}, [#{title(month_rows)}]), [##{typst_months(month_rows)})]
-              )
-            TYPST
-          end
-
-          private
-
-          def title(month_rows)
+          def title(month_rows, _page_number)
             first = month_rows.first.first
             last = month_rows.last.last
 
             <<~TYPST
-              #table(
-                columns: (auto, 1fr, auto),
-                rows: 1fr,
-                align: horizon + center,
-                inset: 0mm,
-                stroke: 0mm,
-                [#{i18n.t("calendar.short.month.#{first.name.downcase}")}
-                 #{first.year}
-                 ---
-                 #{i18n.t("calendar.short.month.#{last.name.downcase}")}
-                 #{last.year}
-                ],
-                [],
-                table(
-                  columns: 2,
-                  rows: 1fr,
-                  align: horizon + center,
-                  [Calendar], [Notes]
-                )
-              )
+              [#{i18n.t("calendar.short.month.#{first.name.downcase}")}
+               #{first.year}
+               ---
+               #{i18n.t("calendar.short.month.#{last.name.downcase}")}
+               #{last.year}
+              ]
             TYPST
           end
 
-          def typst_months(month_rows)
+          def content(month_rows, _page_number)
             <<~TYPST
               stack(
                 dir: ttb,
@@ -67,37 +37,6 @@ module LatexYearlyPlanner
                 #{months.map { |month| Xtypst::LittleCalendar.new(month, **params.object(:little_calendar)).to_typst }.join(', ')}
               )
             TYPST
-          end
-
-          def temp
-            <<~TYPST
-              rotate(
-                  #{mosnav[:rotate]},
-                  origin: center + horizon,
-                  reflow: true,
-                  [
-                  #table(
-                  columns: (#{(['1fr'] * 4).join(', ')}, auto, #{(['1fr'] * 12).join(', ')}),
-                  rows: 1fr,
-                  align: horizon + center,
-                  #{mosnav_content}
-                  )]
-                )
-            TYPST
-          end
-
-          def mosnav_content
-            quarters = params.quarters.map { |q| "[#{i18n.t('calendar.one_letter.quarter')}#{q.number}]" }
-            months = params.months.map { |m| "[#{i18n.t("calendar.short.month.#{m.name.downcase}")}]" }
-
-            if mosnav[:reverse_array_internals]
-              quarters.reverse!
-              months.reverse!
-            end
-
-            return "#{quarters.join(', ')}, [], #{months.join(', ')}" unless mosnav[:reverse_arrays]
-
-            "#{months.join(', ')}, [], #{quarters.join(', ')}"
           end
         end
       end
