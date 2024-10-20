d187807085a68bc662c5933b82cc05fd90f4deda
Vitaliy Kudryk <vitaliy@bgnfu7re.me>
2024-10-20 17:08:47 +0300

    groom

--- lib/latex_yearly_planner/planners/typst_mos/pages/annual.rb
+++ lib/latex_yearly_planner/planners/typst_mos/pages/annual.rb
@@ -27,16 +27,6 @@ module LatexYearlyPlanner
             "vert_stack_bottom_outset(#{month_rows.map { |row| row_stack(row) }.append('[]').join(', ')})"
           end
 
-          def row_stack(months)
-            "stack(dir: ltr, spacing: 1fr, #{months_row(months)})"
-          end
-
-          def months_row(months)
-            months
-              .map { |month| Xtypst::LittleCalendar.new(month, **params.object(:little_calendar)).to_typst }
-              .join(', ')
-          end
-
           def highlight_calendar?
             true
           end
@@ -65,6 +55,16 @@ module LatexYearlyPlanner
 
           def page_number
             (params.months.find_index(month_rows.first.first) / params.get(:months_per_page)) + 1
+          end
+
+          def row_stack(months)
+            "stack(dir: ltr, spacing: 1fr, #{months_row(months)})"
+          end
+
+          def months_row(months)
+            months
+              .map { |month| Xtypst::LittleCalendar.new(month, **params.object(:little_calendar)).to_typst }
+              .join(', ')
           end
         end
       end
