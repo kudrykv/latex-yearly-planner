faab0895ed51d31756b2b8191ba2742ef7c0857e
Vitaliy Kudryk <vitaliy@bgnfu7re.me>
2024-10-21 07:36:14 +0300

    experiment

--- lib/latex_yearly_planner/planners/typst_mos/pages/daily.rb
+++ lib/latex_yearly_planner/planners/typst_mos/pages/daily.rb
@@ -39,64 +39,93 @@ module LatexYearlyPlanner
           def content
             <<~TYPST
               grid(
-                columns: (#{params.get(:schedule_width)}, #{params.get(:gap_width)}, 1fr),
-                stack(
-                  dir: ttb,
-                  spacing: 5mm,
-                  table(
-                    columns: 1fr,
-                    inset: 0mm,
-                    stroke: (_, y) =>
-                      if calc.even(y) { ( bottom: 0.4pt + black ) }
-                      else { ( bottom: 0.4pt + gray ) },
-                    table.cell(stroke: (bottom: 1pt), box(height: 5mm, align(horizon, [#{i18n.t('schedule')}]))),
-                    #{schedule},
-                    #{params.get(:schedule_trailing_30min) ? 'box(height: 5mm)' : ''}
-                  ),
-                  #{maybe_little_calendar}
-                ),
+                columns: (#{params.get(:left_column_width)}, #{params.get(:gap_width)}, 1fr),
+                #{left_column},
                 [],
-                stack(
-                  dir: ttb,
-                  spacing: 5mm,
-                  table(
-                    columns: 1fr,
-                    inset: 0mm,
-                    stroke: (_, _) => (bottom: 0.4pt + black),
-                    table.cell(stroke: (bottom: 1pt), box(height: 5mm, align(horizon, [#{i18n.t('top_priorities')}]))),
-                    #{top_priorities}
-                  ),
-
-                  box(height: 5mm, width: 100%, stroke: (bottom: 1pt), align(horizon, [#{i18n.t('daily_notes')}])),
-                  box(height: #{params.get(:notes_height)}, width: 100%, rect_pattern(#{params.get(:pattern)})),
-
-                  box(height: 5mm, width: 100%, stroke: (bottom: 1pt), align(horizon, [#{i18n.t('personal_notes')}])),
-                  box(height: #{params.get(:personal_notes_height)}, width: 100%, rect_pattern(#{params.get(:pattern)})),
-                )
+                #{right_column}
               )
             TYPST
           end
 
           private
 
-          def schedule
-            (params.get(:schedule_from_hour)..params.get(:schedule_to_hour)).map do |hour|
-              <<~TYPST
-                box(height: 5mm, align(horizon, [#{hour}])), box(height: 5mm)
-              TYPST
-            end.join(",\n")
-          end
-
-          def maybe_little_calendar
-            return '' unless params.get(:enable_little_calendar)
-
+          def left_column
             <<~TYPST
-              #{Xtypst::LittleCalendar.new(day.month, i18n:, **params.object(:little_calendar)).to_typst},
+              stack(dir: ttb, spacing: 5mm, #{run_methods_of(:left_column_items)})
             TYPST
           end
 
+          def right_column
+            <<~TYPST
+              stack(dir: ttb, spacing: 5mm, #{run_methods_of(:right_column_items)})
+            TYPST
+          end
+
+          def run_methods_of(column)
+            params.get(column).map(&method(:send)).join(",\n")
+          end
+
+          def schedule
+            <<~TYPST
+              table(
+                columns: 1fr,
+                inset: 0mm,
+                stroke: (_, y) =>
+                  if calc.even(y) { ( bottom: 0.4pt + black ) }
+                  else { ( bottom: 0.4pt + gray ) },
+                table.cell(stroke: (bottom: 1pt), box(height: 5mm, align(horizon, [#{i18n.t('schedule')}]))),
+                #{schedule_lines},
+                #{params.get(:schedule_trailing_30min) ? 'box(height: 5mm)' : ''}
+              )
+            TYPST
+          end
+
+          def schedule_lines
+            (params.get(:schedule_from_hour)..params.get(:schedule_to_hour)).map do |hour|
+              "box(height: 5mm, align(horizon, [#{hour}])), box(height: 5mm)"
+            end.join(",\n")
+          end
+
+          def little_calendar
+            Xtypst::LittleCalendar.new(day.month, i18n:, **params.object(:little_calendar)).to_typst
+          end
+
           def top_priorities
+            <<~TYPST
+              table(
+                columns: 1fr,
+                inset: 0mm,
+                stroke: (_, _) => (bottom: 0.4pt + black),
+                table.cell(stroke: (bottom: 1pt), box(height: 5mm, align(horizon, [#{i18n.t('top_priorities')}]))),
+                #{top_priorities_lines}
+              )
+            TYPST
+          end
+
+          def top_priorities_lines
             (['box(height: 5mm, align(horizon, [$square.stroked$]))'] * params.get(:priorities_number)).join(",\n")
+          end
+
+          def notes
+            <<~TYPST
+              stack(
+                dir: ttb,
+                spacing: 5mm,
+                box(height: 5mm, width: 100%, stroke: (bottom: 1pt), align(horizon, [#{i18n.t('daily_notes')}])),
+                box(height: #{params.get(:notes_height)}, width: 100%, rect_pattern(#{params.get(:pattern)})),
+              )
+            TYPST
+          end
+
+          def personal_notes
+            <<~TYPST
+              stack(
+                dir: ttb,
+                spacing: 5mm,
+                box(height: 5mm, width: 100%, stroke: (bottom: 1pt), align(horizon, [#{i18n.t('personal_notes')}])),
+                box(height: #{params.get(:personal_notes_height)}, width: 100%, rect_pattern(#{params.get(:pattern)})),
+              )
+            TYPST
           end
         end
       end
