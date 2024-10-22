05602f9377efac572db400260b6172dead209294
Vitaliy Kudryk <vitaliy@bgnfu7re.me>
2024-10-22 08:50:13 +0300

    experiment

--- lib/latex_yearly_planner/adapters/typst_compiler.rb
+++ lib/latex_yearly_planner/adapters/typst_compiler.rb
@@ -7,15 +7,26 @@ module LatexYearlyPlanner
     class TypstCompiler
       attr_reader :workdir, :planner_config
 
+      OUTPUT_FILE = 'index.pdf'
+      TEMP_FILE = 'temp.pdf'
+
       def initialize(workdir:, planner_config:)
         @workdir = workdir
         @planner_config = planner_config
       end
 
       def compile(text_document)
-        `typst compile #{workdir}/#{text_document.name}`
+        `typst compile #{workdir}/#{text_document.name} #{workdir}/#{OUTPUT_FILE}`
 
         raise "Failed to compile #{text_document.name}" unless $CHILD_STATUS.success?
+
+        run_ghostscript
+      end
+
+      private
+
+      def run_ghostscript
+        return unless planner_config.config.dig(:compiler, :ghostscript, :enable)
 
         `gs \
           -sDEVICE=pdfwrite \
@@ -24,10 +35,12 @@ module LatexYearlyPlanner
           -dQUIET \
           -dBATCH \
           -dAutoRotatePages=/None \
-          -sOutputFile=#{workdir}/output.pdf \
-          #{workdir}/index.pdf`
+          -sOutputFile=#{workdir}/#{TEMP_FILE} \
+          #{workdir}/#{OUTPUT_FILE}`
 
         raise 'Failed to slim PDF' unless $CHILD_STATUS.success?
+
+        `mv #{workdir}/#{TEMP_FILE} #{workdir}/#{OUTPUT_FILE}`
       end
     end
   end
