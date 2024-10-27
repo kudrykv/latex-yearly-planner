485eb5716018068cb36fcc654fafe58d2452492d
Vitaliy Kudryk <vitaliy@bgnfu7re.me>
2024-10-27 18:59:06 +0200

    refactor

--- lib/latex_yearly_planner/planners/typst_mos/pages/face.rb
+++ lib/latex_yearly_planner/planners/typst_mos/pages/face.rb
@@ -64,104 +64,6 @@ module LatexYearlyPlanner
 
           private
 
-          def header_layout
-            <<~TYPST
-              table(
-                columns: (auto, 1fr, auto),
-                rows: 1fr,
-                align: horizon + center,
-                inset: 0mm,
-                stroke: 0mm,
-                #{title}, [], #{menu_items_layout}
-              )
-            TYPST
-          end
-
-          def menu_items_layout
-            return '[]' if menu_items_content.empty?
-
-            <<~TYPST
-              table(
-                  stroke: (x, y) => (left: 0.4pt, right: 0.4pt),
-                  columns: #{menu_items_content.size},
-                  rows: 1fr,
-                  align: horizon + center,
-                  #{menu_items_content.join(', ')}
-                )
-            TYPST
-          end
-
-          def menu_items_content
-            @menu_items_content ||= if heading[:put_extra_items] == 'left'
-              extra_menu_items.push(annual_menu_item, todo_menu_item, note_menu_item).compact
-            else
-              [annual_menu_item, todo_menu_item,
-               note_menu_item].concat(extra_menu_items).compact
-            end
-          end
-
-          def annual_menu_item
-            return nil unless params.section_enabled?(:annual)
-
-            name = i18n.t('menu_calendar')
-
-            return "link(<annual-#{annual_page_number(top_menu_month)}>, [#{name}])" if top_menu_month
-            return "table.cell(fill: black, link(<annual-1>, text(white)[#{name}]))" if highlight_calendar?
-
-            "link(<annual-1>, [#{name}])"
-          end
-
-          def todo_menu_item
-            return nil unless params.section_enabled?(:todo_index)
-            return nil if hide_todo?
-
-            name = i18n.t('todo.menu_index')
-
-            "link(<ti-1>, [#{name}])"
-          end
-
-          def note_menu_item
-            return nil unless params.section_enabled?(:notes_index)
-            return nil if hide_notes?
-
-            name = i18n.t('notes.menu_index')
-
-            "link(<ni-1>, [#{name}])"
-          end
-
-          def annual_page_number(first_month)
-            annual_params = params.planner_config.sections
-                                  .find { |section| section.name == :annual }
-                                  .params
-            months_per_page = annual_params.get(:months_per_page)
-
-            (annual_params.months.find_index(first_month) / months_per_page) + 1
-          end
-
-          def side_menu_layout
-            side_menu_object.to_typst
-          end
-
-          def make_side_menu_quarters
-            params.quarters.map do |q|
-              name = i18n.t('calendar.one_letter.quarter')
-
-              next "link(<#{q.id}>, [#{name}#{q.number}])" unless highlight_side_menu_quarters.include?(q)
-
-              "table.cell(fill: black, link(<#{q.id}>, text(white)[#{name}#{q.number}]))"
-            end
-          end
-
-          def make_side_menu_months
-            params.months.map do |m|
-              name = i18n.t("calendar.short.month.#{m.name.downcase}")
-
-              next "link(<#{m.id}>, [#{name}])" unless highlight_side_menu_months.include?(m)
-
-              "table.cell(fill: black, link(<#{m.id}>, text(white)[#{name}]))"
-            end
-          end
-
           def heading_columns
             columns = [mos_layout[:mos_nav_width], '1fr']
             columns.reverse! if mos_layout[:side_menu] == 'right'
@@ -174,10 +76,6 @@ module LatexYearlyPlanner
             row.reverse! if mos_layout[:side_menu] == 'right'
 
             row.join(', ')
-          end
-
-          def heading
-            params.object(:heading)
           end
 
           def mos_layout
