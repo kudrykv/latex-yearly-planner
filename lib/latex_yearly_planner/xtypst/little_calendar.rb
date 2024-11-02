320c98a3b3749f52d770278edb0e14b2f01c6328
Vitaliy Kudryk <vitaliy@bgnfu7re.me>
2024-11-02 20:27:55 +0200

    refactor

--- lib/latex_yearly_planner/xtypst/little_calendar.rb
+++ lib/latex_yearly_planner/xtypst/little_calendar.rb
@@ -36,7 +36,7 @@ module LatexYearlyPlanner
             stroke: 0mm,
             #{highlight_week}
             table.cell(colspan: #{number_of_columns})[#{month_name}],
-            #{LittleWeekdaysRow.new(weekday_start: month.weekday_start, i18n:, **parameters).to_typst},
+            #{weekdays_row},
             #{weeks}
           )
         TYPST
@@ -72,48 +72,7 @@ module LatexYearlyPlanner
       end
 
       def weekdays_row
-        apply_underline_weekdays!
-        return weekdays_row_internal unless parameters[:with_week_numbers]
-
-        apply_week_number_placement!
-        apply_sideline_week_numbers!
-
-        weekdays_row_internal
-      end
-
-      def weekdays_row_internal
-        @weekdays_row_internal ||= rotated_weekdays.map(&method(:one_letter_day))
-      end
-
-      def apply_week_number_placement!
-        return weekdays_row_internal.unshift(one_letter_week) if parameters[:week_number_placement] == 'left'
-
-        weekdays_row_internal.append(one_letter_week)
-      end
-
-      def apply_sideline_week_numbers!
-        return unless parameters[:sideline_week_numbers]
-
-        x = parameters[:week_number_placement] == 'left' ? 1 : 7
-        weekdays_row_internal.unshift("table.vline(x: #{x}, stroke: 0.4pt)")
-      end
-
-      def apply_underline_weekdays!
-        return unless parameters[:underline_weekdays]
-
-        weekdays_row_internal.unshift('table.hline(y: 2, stroke: 0.4pt)')
-      end
-
-      def one_letter_day(day)
-        "[#{i18n.t("calendar.one_letter.#{day}")}]"
-      end
-
-      def one_letter_week
-        "[#{i18n.t('calendar.one_letter.week')}]"
-      end
-
-      def rotated_weekdays
-        WEEKDAYS.rotate(WEEKDAYS.index(month.weekday_start))
+        LittleWeekdaysRow.new(weekday_start: month.weekday_start, i18n:, **parameters).to_typst
       end
 
       def weeks
