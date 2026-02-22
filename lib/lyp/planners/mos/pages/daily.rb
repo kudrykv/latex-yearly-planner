39701487fb3a39fa46b5da513b2ce5eea174e4ca
Vitaliy Kudryk <vitaliy@bgnfu7re.me>
2026-02-22 17:25:42 +0300

    wip

--- lib/lyp/planners/mos/pages/daily.rb
+++ lib/lyp/planners/mos/pages/daily.rb
@@ -5,13 +5,14 @@ module LYP
     module MOS
       module Pages
         class Daily
-          attr_accessor :i18n, :manifest, :day, :debug
+          attr_accessor :i18n, :manifest, :day, :debug, :params
 
-          def initialize(i18n:, manifest:, day:, debug: false)
+          def initialize(i18n:, manifest:, day:, debug: false, **params)
             self.i18n = i18n
             self.manifest = manifest
             self.day = day
             self.debug = debug
+            self.params = params
           end
 
           def title
@@ -38,7 +39,51 @@ module LYP
           end
 
           def content
-            "[haha]"
+            <<~TYPST
+              grid(
+                columns: (auto, auto),
+                rows: 1fr,
+                [#{left_column}]
+              )
+            TYPST
+          end
+
+          private
+
+          def left_column
+            params[:left_column].map do |comp|
+              next unless comp[:enabled]
+
+              case comp[:class]
+              when "schedule"
+                my_schedule(**comp[:params])
+              end
+            end.join
+          end
+
+          def my_schedule(from:, to:, strftime:, trailing_30_minutes:)
+            <<~TYPST
+              #grid(
+                columns: 1fr,
+                inset: 0mm,
+                stroke: (_, y) =>
+                  if calc.even(y) { ( bottom: 0.4pt + black ) }
+                  else { ( bottom: 0.4pt + gray ) },
+                grid.cell(stroke: (bottom: 1pt), box(height: 5mm, align(horizon, [#{i18n.t('schedule')}]))),
+                #{schedule_lines(from:, to:, strftime:)},
+                #{trailing_30_minutes ? 'box(height: 5mm)' : ''}
+              )
+            TYPST
+          end
+
+          def schedule_lines(from:, to:, strftime:)
+            (from..to).map do |hour|
+              "box(height: 5mm, align(horizon, [#{pretty_hour(hour:, strftime:)}])), box(height: 5mm)"
+            end.join(",\n")
+          end
+
+          def pretty_hour(hour:, strftime:)
+            DateTime.parse("#{hour}:00").strftime(strftime)
           end
         end
       end
--- lib/lyp/planners/mos/sections/daily.rb
+++ lib/lyp/planners/mos/sections/daily.rb
@@ -5,11 +5,12 @@ module LYP
     module MOS
       module Sections
         class Daily
-          attr_accessor :i18n, :overseer
+          attr_accessor :i18n, :overseer, :params
 
-          def initialize(i18n:, overseer:, **_rest)
+          def initialize(i18n:, overseer:, **params)
             self.i18n = i18n
             self.overseer = overseer
+            self.params = params
           end
 
           def register(manifest)
@@ -37,7 +38,7 @@ module LYP
           end
 
           def page(manifest, day)
-            Pages::Daily.new(i18n:, manifest:, day:, debug: overseer.debug?)
+            Pages::Daily.new(i18n:, manifest:, day:, debug: overseer.debug?, **params)
           end
         end
       end
