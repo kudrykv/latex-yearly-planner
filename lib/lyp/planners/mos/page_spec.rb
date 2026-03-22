69dfddac6158e9ab50827d45f12b30b277f17441
Vitaliy Kudryk <vitaliy@bgnfu7re.me>
2026-03-22 18:02:22 +0300

    wip

---
+++ lib/lyp/planners/mos/page_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module LYP
+  module Planners
+    module MOS
+      PageSpec = Data.define(:raw_typst?, :title, :content, :highlight_months, :highlight_quarters) do
+        def initialize(content:, raw_typst: false, title: nil, highlight_months: [], highlight_quarters: [])
+          super
+        end
+
+        def raw_typst? = raw_typst
+      end
+    end
+  end
+end
