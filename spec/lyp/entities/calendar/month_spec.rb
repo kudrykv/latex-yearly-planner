e0b91888f9dee42ba64f998c7b07faacdbbc8692
Vitaliy Kudryk <vitaliy@bgnfu7re.me>
2026-03-26 10:26:12 +0300

    wip

--- spec/lyp/entities/calendar/month_spec.rb
+++ spec/lyp/entities/calendar/month_spec.rb
@@ -8,4 +8,26 @@ RSpec.describe LYP::Entities::Calendar::Month do
       expect(month.weekday_start).to eq(:monday)
     end
   end
+
+  describe "#id" do
+    it "returns month- prefixed date id" do
+      expect(make_month("2026-03").id).to eq("month-2026-03-01")
+      expect(make_month("2026-12").id).to eq("month-2026-12-01")
+    end
+  end
+
+  describe "#name" do
+    it "returns lowercase month name" do
+      expect(make_month("2026-03").name).to eq("march")
+      expect(make_month("2026-12").name).to eq("december")
+    end
+  end
+
+  describe "#quarter" do
+    it "returns quarter for the month" do
+      expect(make_month("2026-03").quarter).to eq(make_quarter("2026-01-01"))
+      expect(make_month("2026-04").quarter).to eq(make_quarter("2026-04-01"))
+      expect(make_month("2026-12").quarter).to eq(make_quarter("2026-10-01"))
+    end
+  end
 end
