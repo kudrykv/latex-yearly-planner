1a2b94146db69b4c886c53ad5ae9bd1c14117d9d
Vitaliy Kudryk <vitaliy@bgnfu7re.me>
2026-04-11 19:27:57 +0300

    rubocop

--- lib/lyp/planners/mos/components/little_calendar.rb
+++ lib/lyp/planners/mos/components/little_calendar.rb
@@ -7,7 +7,7 @@ module LYP
         class LittleCalendar
           attr_reader :i18n, :manifest, :week_placement, :month, :show_month_name, :today, :inset
 
-          def initialize(i18n:, manifest:, week_placement:, month:, day: nil, show_month_name: false, inset:, **_rest)
+          def initialize(i18n:, manifest:, week_placement:, month:, inset:, day: nil, show_month_name: false, **_rest)
             @i18n = i18n
             @manifest = manifest
             @week_placement = week_placement.to_sym
