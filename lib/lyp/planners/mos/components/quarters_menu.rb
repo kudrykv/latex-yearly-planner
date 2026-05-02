340d6c66a96e7d001e81c31922017fb05da65c82
Vitaliy Kudryk <vitaliy@bgnfu7re.me>
2026-05-02 12:11:13 +0200

    simplify one thing, probably

--- lib/lyp/planners/mos/components/quarters_menu.rb
+++ lib/lyp/planners/mos/components/quarters_menu.rb
@@ -19,7 +19,7 @@ module LYP
           def generate
             <<~TYPST.strip
               table(
-                stroke: (x, y) => (left: regular_stroke, right: regular_stroke, bottom: regular_stroke),
+                stroke: regular_stroke,
                 columns: (#{(["1fr"] * range.count).join(", ")}),
                 rows: 1fr,
                 align: horizon + center,
